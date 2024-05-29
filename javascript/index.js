bae9451c6a79fe0f8040248040455ea22cc9c52c
Zaccaria <zac.fede96@gmail.com>
2024-05-29 15:48:23 +0200

    scope variabili

--- javascript/index.js
+++ javascript/index.js
@@ -12,3 +12,9 @@ console.log(num3);
 num3 = num1 + parseInt(num2);
 console.log(num3);
 
+//Es 3:                                                                                       Scope Delle Variabili
+let number = 100;     //scope globale=definita globalmente all'internodel nostro file         la variabile number all'interno delle parentesi 
+{let number = 15;    // scope locale= disponibile solo all'interno del codice tra graffe      esisterà e avrà quel valore solo li
+    console.log(number)            // quindi disponibile localmente
+}
+console.log(number);
