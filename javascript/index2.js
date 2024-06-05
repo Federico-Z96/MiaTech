1b3a176d7257821f1543067880c10152ca82583b
Zaccaria <zac.fede96@gmail.com>
2024-06-05 11:22:37 +0200

    funzioni annidate modifica

--- javascript/index2.js
+++ javascript/index2.js
@@ -1,10 +1,22 @@
 //Es 21(funzioni annidate):
-function outerFunction() {
-    function innerFunction(x, y) {
+/*function outerFunction(x) {
+    function innerFunction(y) {
         let sum = x + y;
         return sum;
     }
     return innerFunction;
 };
 
-console.log(outerFunction()(3, 5));
+console.log(outerFunction()(3, 5));*/
+
+//Es 22 modifica outerFunction:
+
+function outerFunction(initialValue) {
+    let result = initialValue;
+    function innerFunction(y) {
+       result += y;
+       return result;
+    }
+    return innerFunction;
+};
+console.log(outerFunction(5)(4));
