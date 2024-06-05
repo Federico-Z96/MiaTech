099917db0740a8fddcd5b647f56628bf3878b1af
Zaccaria <zac.fede96@gmail.com>
2024-06-05 11:30:30 +0200

    counter

--- javascript/index2.js
+++ javascript/index2.js
@@ -20,3 +20,23 @@ function outerFunction(initialValue) {
     return innerFunction;
 };
 console.log(outerFunction(5)(4));
+
+//Es 23(counter):
+function createCounter(){
+    let count = 0;
+    return {
+        increment : function() {
+            count++;
+            return count
+        },
+        decrement : function(){
+            count--;
+            return count;
+        }
+    }
+}
+const counter = createCounter();
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.decrement());
