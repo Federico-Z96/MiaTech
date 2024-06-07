70e535c3d06c5e75e19807a8d5f6573533b8d4f1
Zaccaria <zac.fede96@gmail.com>
2024-06-07 14:32:57 +0200

    forEach e .map

--- javascript/index2.js
+++ javascript/index2.js
@@ -61,3 +61,14 @@ console.log(Object.values(student));
 
 console.log(Object.entries(student));
 
+//Es 27:
+
+const numbers = [ 1, 2, 3, 4, 5 ];
+
+numbers.forEach(numbers => {                   //forEach va a creare un iterazione di tutti gli elementi di numers, andandoli a moltiplicare per 2 prima di vederli rappresentati in console
+    console.log(numbers * 2);
+});
+
+const squaredNumbers = numbers.map(numbers => numbers * numbers);  // map va ad attribuire gli elementi di numbers al quadrato al nuovo array creato(squaredNumbers)
+
+console.log(squaredNumbers);
