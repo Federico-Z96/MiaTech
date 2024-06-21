54323e1ccc5e98ff9ecd7129c1c0e92a8fee345d
Zaccaria <zac.fede96@gmail.com>
2024-06-21 14:28:17 +0200

    destructuring array

---
+++ javascript/index3.js
@@ -0,0 +1,10 @@
+//Es 44(destructuring):
+
+let arrayDiNumeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let primoNumero = arrayDiNumeri[0];
+let secondoNumero = arrayDiNumeri[1];
+
+console.log(primoNumero);
+console.log(secondoNumero);
+
