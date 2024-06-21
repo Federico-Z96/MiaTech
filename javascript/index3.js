74a730608a7d8c7c94ae65a590dd6953237ca71c
Zaccaria <zac.fede96@gmail.com>
2024-06-21 14:54:46 +0200

    destructuring con elementi con valori predefiniti

--- javascript/index3.js
+++ javascript/index3.js
@@ -8,9 +8,26 @@ let secondoNumero = arrayDiNumeri[1];
 console.log(primoNumero);
 console.log(secondoNumero);
 
+const [primoNumer, secondoNumer] = arrayDiNumeri;
 
+console.log("Primo numero:", primoNumer);
+console.log("Secondo numero:", secondoNumer);
 
 //Es 45:
 
 let [numeroUno, , numeroTre] = arrayDiNumeri;
 console.log(numeroUno, numeroTre);
+
+//Es 46:
+
+const numeri = [10, 20, 30];
+
+// Destructuring con valori predefiniti
+const [primoNumeroo , secondoNumeroo , terzoNumero , quartoNumero = 0, quintoNumero = 0] = numeri;
+
+// Stampa delle variabili nella console
+console.log("Primo numero:", primoNumeroo);
+console.log("Secondo numero:", secondoNumeroo);
+console.log("Terzo numero:", terzoNumero);
+console.log("Quarto numero:", quartoNumero);
+console.log("Quinto numero:", quintoNumero); 
