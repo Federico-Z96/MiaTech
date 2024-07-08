9becc1c466bab548552868af03a8b94d1a864bab
Zaccaria <zac.fede96@gmail.com>
2024-07-08 14:11:02 +0200

    funzione asincrona che chiama due funzioni

--- javascript/index5.js
+++ javascript/index5.js
@@ -1,6 +1,6 @@
 //Es 84(funzione asincrona):
 
-const promessa = () => {
+/* const promessa = () => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve("Promessa risolta dopo 2,5 secondi");
@@ -34,5 +34,44 @@ const funzioneAsincrona = async () => {
     }
 };
 
-funzioneAsincrona();
+funzioneAsincrona();*/
 
+//Es 86(funzione asincrona in serie):
+
+const fun1 = (a) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(typeof a ==="number"){
+                 resolve(a);
+                 console.log("A è un numero");
+            } else {
+                 reject("A non è un numero");
+            }  
+        }, 1000);
+    });
+};
+
+const fun2 = (num) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            if(num < 50) {
+              resolve("il numero è minore di 50");  
+            } else {
+              resolve("il numero è maggiore o uguale a 50");  
+            }
+        }, 2000);
+    });
+};
+
+const funRichiamo = async (a) => {
+    try {
+        const result = await fun2(await fun1(a));
+        console.log(result);
+    } catch(error) {
+        console.error(error);
+    }
+};
+
+funRichiamo(30);
+funRichiamo("a");
+funRichiamo(51);
