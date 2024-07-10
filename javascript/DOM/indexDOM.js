829606cfd0ecf85ddebcd110c27c9cda3907fa23
Zaccaria <zac.fede96@gmail.com>
2024-07-10 20:05:12 +0200

    get element by id

--- javascript/DOM/indexDOM.js
+++ javascript/DOM/indexDOM.js
@@ -1,3 +1,8 @@
 // Es 97(console.log document):
 
 console.log("il DOM è: ", document);
+
+//Es 98(get element by id):
+
+const headerElement = document.getElementById('header');
+console.log('Elemento selezionato tramite getElementById:', headerElement);
