889324af51c33fb8e2fdb226f9b24eee568d1ced
Zaccaria <zac.fede96@gmail.com>
2024-07-10 19:42:38 +0200

    session storage

--- javascript/index5.js
+++ javascript/index5.js
@@ -229,7 +229,7 @@ function recuperaCookie(nome) {
 
 //Es 94(local storage):
 
-const bottonePizza = document.getElementById("bottone-pizza");
+/* const bottonePizza = document.getElementById("bottone-pizza");
 const rimuoviPizza = document.getElementById("rimuovi-pizza");
 const recuperaPizza = document.getElementById("recupera-pizza");
 const paragrafoPizza = document.getElementById("pizza");
@@ -252,6 +252,35 @@ rimuoviPizza.addEventListener("click", function() {
 
 recuperaPizza.addEventListener("click", function() {
     const nomePizza = localStorage.getItem("my_preferred_pizza") || "Nessuna pizza salvata";
+    paragrafoPizza.innerText = nomePizza;
+    console.log(`Recuperato: ${nomePizza}`);
+});*/
+
+//es 95 (session storage):
+
+const bottonePizza = document.getElementById("bottone-pizza");
+const rimuoviPizza = document.getElementById("rimuovi-pizza");
+const recuperaPizza = document.getElementById("recupera-pizza");
+const paragrafoPizza = document.getElementById("pizza");
+
+const nomePizza = sessionStorage.getItem("my_preferred_pizza") || "";
+paragrafoPizza.innerText = nomePizza;
+console.log(`Recuperato all'avvio: ${nomePizza}`);
+
+bottonePizza.addEventListener("click", function() {
+    const nomePizza = prompt("qual'è la tua pizza preferita?", "nome pizza");
+    sessionStorage.setItem("my_preferred_pizza", nomePizza);
+    paragrafoPizza.innerText = nomePizza
+});
+
+rimuoviPizza.addEventListener("click", function() {
+    sessionStorage.removeItem("my_preferred_pizza");
+    paragrafoPizza.innerText = "";
+    console.log("Pizza preferita rimossa");
+});
+
+recuperaPizza.addEventListener("click", function() {
+    const nomePizza = sessionStorage.getItem("my_preferred_pizza") || "Nessuna pizza salvata";
     paragrafoPizza.innerText = nomePizza;
     console.log(`Recuperato: ${nomePizza}`);
 });
