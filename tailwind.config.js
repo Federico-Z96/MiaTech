dd6c12145f420ebd0c0962f60456db4d999630ad
Zaccaria <zac.fede96@gmail.com>
2024-07-12 15:43:43 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {colors: {
+      primary: "#41463D",
+      secondary: "rgb(15 23 42)",
+  },
+},
+  },
+  plugins: [],
+}
+
