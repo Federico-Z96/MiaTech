c6433b5a47d488e0527e816ed225190ac2697e7c
Zaccaria <zac.fede96@gmail.com>
2024-07-26 15:41:03 +0200

    Counter button increment

--- react-exercise/src/components/Counter.js
+++ react-exercise/src/components/Counter.js
@@ -3,7 +3,16 @@ import {useState} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0);
-    return (<div>Count: {count} </div>)
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+    return (
+        <div>
+             <div>Count: {count} </div>
+             <button onClick={handleIncrement}>+</button>  
+       </div>
+    )
 }
 
 export default Counter;
