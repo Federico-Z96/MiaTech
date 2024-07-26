5595f00b2f5746ced264ee4dc498ed492f624c9c
Zaccaria <zac.fede96@gmail.com>
2024-07-26 13:56:19 +0200

    COLORI E TESTO

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,28 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {colors: {
-      primary: "#41463D",
-      secondary: "rgb(15 23 42)",
-      tertiary: " #000035",
-      "custom-blue": "blue",
-      primary: "green",
-      purple: '#7e5bef',
-      pink: '#ff49db',
-      orange: '#ff7849',
-      yellow: '#ffc82c',
-      "custom-green": '#10B981',
+    extend: {
+      colors: {
+         primary: "#41463D",
+         secondary: "rgb(15 23 42)",
+         tertiary: " #000035",
+        "custom-blue": "blue",
+         primary: "green",
+         purple: '#7e5bef',
+         pink: '#ff49db',
+         orange: '#ff7849',
+         yellow: '#ffc82c',
+        "custom-green": '#10B981',
         "custom-red": '#EF4444',
-  },
-  margin: {
-    first: "50%",
-  },
-  padding: {
-    pFirst: "50px"
-  },
+        "custom-yellow": '#F59E0B',
+        "custom-purple": '#8B5CF6',
+      },
+       margin: {
+          first: "50%",
+      },
+       padding: {
+          pFirst: "50px"
+      },
 },
   },
   plugins: [],
