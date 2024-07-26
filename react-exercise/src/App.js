5e0a6e5a22df90bff0a630c7f2a0daae82a7ee7a
Zaccaria <zac.fede96@gmail.com>
2024-07-26 15:33:19 +0200

    Counter

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -1,9 +1,11 @@
 import HelloWorld from "./components/HelloWorld";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <HelloWorld />
+       <Counter />
     </div>
   );
 }
